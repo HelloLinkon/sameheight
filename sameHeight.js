a7b7037f8dcab289cf958fa69e845d3b001b57a7
linkon <linkon@linkons-MacBook-Air.local>
2016-07-01 03:28:32 +0600

    add responsiveness

--- sameHeight.js
+++ sameHeight.js
@@ -1,9 +1,20 @@
-(function(){
+(function($, window){
 		'use strict';
 
 		$.fn.sameHeight = function(){
-			var $div = this;
 			
+			var $this = this;
+			changehight($this);
+
+		    $(window).resize(function() {
+		    	changehight($this);
+		    });
+
+		    return this;
+
+		};
+
+		function changehight($div){
 			var highestHieght = 0;
 			$div.each(function(index, value){
 		    	 
@@ -12,11 +23,8 @@
 
 		    });
 
-		    console.log(highestHieght);
+		    
 		    $div.css("min-height", highestHieght);
+		}
 
-		    return this;
-
-			};
-
-})();
+})(jQuery, window);
