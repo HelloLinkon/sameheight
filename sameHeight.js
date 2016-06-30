10b833ebba4e3f0bfd698a2171c7fce38da59e8e
linkon <linkon@linkons-MacBook-Air.local>
2016-07-01 04:04:01 +0600

    add option

--- sameHeight.js
+++ sameHeight.js
@@ -1,19 +1,33 @@
 (function($, window){
 		'use strict';
 
-		$.fn.sameHeight = function(){
+		$.fn.sameHeight = function(options){
 			
+			// Default options
+	        var settings = $.extend({
+	            way: 'max'
+	        }, options );
+
+
 			var $this = this;
-			changehight($this);
+
+			if(settings.way == "max")
+				changehight($this);
+			else
+				changehightTwo($this);
 
 		    $(window).resize(function() {
-		    	changehight($this);
-		    });
+		    	if(settings.way == "max")
+				changehight($this);
+				else
+					changehightTwo($this);
+			    });
 
 		    return this;
 
 		};
 
+		//based on max height
 		function changehight($div){
 			var highestHieght = 0;
 			$div.each(function(index, value){
@@ -25,6 +39,25 @@
 
 		    
 		    $div.css("min-height", highestHieght);
+		}
+
+		//based on min height
+		function changehightTwo($div){
+			console.log($($div[0]).outerHeight());
+			var lowestHieght = $($div[0]).outerHeight();
+			$div.each(function(index, value){
+		    	 
+		    	let elHeight = $(value).outerHeight();
+		    	lowestHieght = (elHeight > lowestHieght)? lowestHieght : elHeight;
+
+		    });
+
+			var styles = {
+		      overflow : "auto",
+		      height : lowestHieght
+		    };
+		    
+		    $div.css( styles );
 		}
 
 })(jQuery, window);
